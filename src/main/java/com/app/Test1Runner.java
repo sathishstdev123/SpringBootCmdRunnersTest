5065ad07a97397d4b462fea299f16869fe41d9be
Sathish <Sathish@LAPTOP-DO9LOK0B>
2019-06-08 16:21:50 +0530

    initial stage

---
+++ src/main/java/com/app/Test1Runner.java
@@ -0,0 +1,14 @@
+package com.app;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+@Component
+public class Test1Runner implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Fromthe a");
+
+	}
+
+}
